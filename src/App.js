e2e0049aec1787da9bd74e8508955592d7d83618
kubraguler <oktnkbr@gmail.com>
2023-08-26 21:41:16 +0100

    Add product card component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import data from "./data/data.json";
+import ProductCard from "./components/ProductCard/ProductCard";
+import ProductDetail from "./components/ProductDetail/ProductDetail";
 import "./App.scss";
 
 function App() {
-	return <div className="App">Hello</div>;
+	const products = data.item.products;
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="/" element={<ProductCard products={products} />} />
+				<Route path="/product-detail" element={<ProductDetail />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,3 +1,34 @@
-// Product Card
+import { Link } from "react-router-dom";
+import "./ProductCard.scss";
 
-// Product Image, Name, Price, InStock, Branc Photo, Favourite, Review, Tag (sale/space saver), BestSeller Tag
+const ProductCard = ({ products }) => {
+	console.log(products);
+	return (
+		<div className="product-list">
+			<main className="product-wrapper">
+				{products.map((product) => (
+					<article className="product" key={product.id}>
+						<img
+							className="product-img"
+							src={product.image?.url}
+							alt={product.image.attributes?.imageAltText}
+						/>
+						<div className="product-content">
+							<Link to="/product-detail">
+								<p className="product-name">{product.productName}</p>
+							</Link>
+							<p className="product-price">
+								<span className="product-currency">
+									{product.price?.currencyCode}
+								</span>
+								{product.price?.priceIncTax}
+							</p>
+						</div>
+					</article>
+				))}
+			</main>
+		</div>
+	);
+};
+
+export default ProductCard;
